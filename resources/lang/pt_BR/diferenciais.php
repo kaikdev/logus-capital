84b1a25dbde7e9b384d153a2272919d410275145
kaikdev <kaikdev.contato@gmail.com>
2026-06-16 01:35:37 -0300

    feat: Create page - Diferenciais

---
+++ resources/lang/pt_BR/diferenciais.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'title1' => 'Diferenciais',
+    'subtitle1' => '
+        Propósito que se traduz em<br>
+        excelência no atendimento
+    ',
+    'item1' => [
+        'title' => 'Propósito que se traduz em excelência no atendimento.',
+        'subtitle' => '<strong>Cultura de dono,</strong> decisões conscientes e relações genuínas: na Logus, <span>performance e cuidado caminham juntos.</span>',
+        'link' => 'Leia mais',
+    ],
+    'item2' => [
+        'title' => 'Decisões mais inteligentes começam com uma visão holística do patrimônio.',
+        'subtitle' => 'Inteligência institucional: o fim da complexidade na gestão de patrimônio',
+        'text' => '
+            A Logus integra tecnologia proprietária e inteligência de gestão para oferecer uma visão consolidada e em tempo real do patrimônio global, transformando dados complexos em decisões estratégicas por meio de um portal exclusivo e aplicativo dedicado.
+            <br>
+            Esse diferencial tecnológico é potencializado por um processo de investimento rigoroso e institucionalizado, desenvolvidos a partir de experiência institucional em mercados internacionais e adaptados à realidade dos clientes no Brasil, que priorizam a descorrelação e a otimização de risco-retorno.
+            <br>
+            Ao substituir a dependência de talentos isolados por modelos analíticos avançados, garantimos transparência, consistência e a geração de valor perene para cada cliente.
+        ',
+    ],
+    'item3' => [
+        'title' => 'Ecossistema Global de Investimentos e Network',
+        'subtitle' => 'Onde a experiência internacional encontra o acesso direto às maiores instituições, <strong>garantindo uma diversificação sem fronteiras e sem conflitos.</strong>',
+        'link' => 'Leia mais',
+    ],
+    'item4' => [
+        'title' => 'Independência que se traduz em paz de espírito',
+        'subtitle' => '
+            Sem interesses ocultos. Sem vieses.
+            <br> 
+            <strong>Apenas decisões alinhadas ao cliente.</strong>
+        ',
+        'link' => 'Leia mais',
+    ],
+    'text' => '
+        Por trás dessa proposta <strong>existe uma estrutura integrada,</strong> que combina tecnologia, consolidação patrimonial, inteligência de mercado, network institucional e apoio especializado, permitindo <strong>transformar visão em execução com consistência.</strong>
+    ',
+    'saiba' => [
+        'title' => 'Saiba sobre a nossa',
+        'subtitle' => 'Atuação',
+        'route' => 'atuacao',
+    ],
+];
